9356e3b0727cdfaed445a25c82f729425eaec4f2
Reaz Patwary <reaz@infixtech.com>
2018-08-18 14:10:02 -0400

    Aug 18

--- src/main/java/com/pom/base/TestBase.java
+++ src/main/java/com/pom/base/TestBase.java
@@ -1,18 +1,59 @@
 package com.pom.base;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import junit.framework.Assert;
 
 public class TestBase {
+	Random ran = new Random();
+	File file;
 	public WebDriver driver;
-
+	public ExtentHtmlReporter htmlReporter;
+	public ExtentReports extent;
+	public ExtentTest testLog;
+	@BeforeClass
+	public void reportInit() {
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"/test-output/STMExtentReport.html");
+		extent = new ExtentReports ();
+		extent.attachReporter(htmlReporter);
+		extent.setSystemInfo("Host Name", "Infixtech.com");
+		extent.setSystemInfo("Environment", "Staging Environment");
+		extent.setSystemInfo("User Name", "Reaz Patwary");
+		
+		htmlReporter.config().setDocumentTitle("Adhock Test Reporter");
+		htmlReporter.config().setReportName("Adhock Test Reporter");
+		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+		htmlReporter.config().setTheme(Theme.STANDARD);
+	}
+	
+	@AfterClass
+	public void reportClose() {
+		extent.flush();
+		
+	}
 	@BeforeMethod
 	public void testStart() {
 		WebDriverManager.chromedriver().setup();
@@ -31,4 +72,17 @@ public class TestBase {
 		// driver.close();
 		// driver.quit();
 	}
+	public void captureScreen() throws IOException{
+        String str = System.getProperty("user.dir") + "/test-output/"; // Current dir
+        System.out.println(str);
+        file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        System.out.println("Screenshot taken to "+ str);
+       // FileUtils.copyFile(file, new File("screenshort"+ran.nextInt()+".png"));
+        FileUtils.copyFile(file, new File("screenshort.png"));
+	}
+	
+	public String getFilePath() {
+		return file.getPath();
+	}
 }
+
--- src/main/java/com/pom/test/MercendiseTest.java
+++ src/main/java/com/pom/test/MercendiseTest.java
@@ -3,17 +3,25 @@ package com.pom.test;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.pom.base.TestBase;
 import com.pom.pages.MerchendisePage;
 
 public class MercendiseTest extends TestBase {
-	
+
+
 	@Test
-	public void sortingByHighToLow() {
+	public void sortingByHighToLow() {		
 		MerchendisePage merchendisePage = new MerchendisePage(driver);
 		merchendisePage.clickMerchendiseLink();
+		testLog.log(Status.PASS, "Clickled on mercendise link");
 		Assert.assertEquals(merchendisePage.mersendiceText().getText(), "Merchandise");
+		testLog.log(Status.PASS, "Verified Merchandise text");
 		merchendisePage.clickDefaultBox();
+		testLog.log(Status.PASS, "Clicked on defualt box");
 	}
 	@Test
 	public void sortingByLowToHigh() {
